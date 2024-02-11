73b09ed0d8d3df18e7282b618848f50e8eb23976
Rond81943 <134205193+Rond81943@users.noreply.github.com>
2024-02-11 13:07:02 +0500

    Add files via upload

--- js/sccript.js
+++ js/sccript.js
@@ -1,51 +1,99 @@
+const buttonElems = document.querySelectorAll('#clear');
+const modalElem = document.querySelector('.modal1');
+const btnClose = '#esc';
+const time = 300;
+const clearBtn = document.querySelector('#ok');
 const myButton = document.querySelector('#btn');
+const searchButton = document.querySelector('#searchBtn');
 const nameInput = document.querySelector('#input');
+const searchInput = document.querySelector('#searchInput');
 const list = document.querySelector('#list');
-
-myButton.disabled = true;
+modalElem.style.cssText = `display: flex; visibility: hidden; opacity: 0; transition: opacity ${time}ms ease-in-out;`;
 
 function isEmpty() {
-    if (nameInput.value != '') {
-        myButton.disabled = false;
-    } else {
-        myButton.disabled = true;
-    }
+    myButton.disabled = nameInput.value ? false : true;
 }
 
 nameInput.addEventListener('keyup', isEmpty);
 
 function add() {
-    let newItem = document.createElement('li');
-    newItem.textContent = nameInput.value;
-    list.appendChild(newItem);
-    newItem.classList.add('item');
+    if (nameInput.value) {
+        let newItem = document.createElement('li');
+        newItem.textContent = nameInput.value;
+        list.appendChild(newItem);
+        newItem.classList.add('item');
 
-    const deleteButton = document.createElement('button');
-    newItem.append(deleteButton);
-    deleteButton.classList.add('delBtn');
-    deleteButton.textContent = 'x';
-    deleteButton.addEventListener('click', () => {
-        list.removeChild(newItem);
-    });
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '&#10006;';
+        deleteButton.classList.add('delBtn');
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(newItem);
+        });
 
-    newItem.appendChild(deleteButton);
-    list.appendChild(newItem);
+        newItem.appendChild(deleteButton);
 
-    nameInput.value = '';
+        nameInput.value = '';
+        isEmpty();
 
-    isEmpty();
-    
-    newItem.addEventListener('click', () => {
-        newItem.classList.toggle('done');
-        
-    });
-};
+        newItem.addEventListener('click', () => {
+            newItem.classList.toggle('done');
+        });
+    }
+}
 
-nameInput.addEventListener('keyup', function (e) {
-    if (e.key === 'Enter') {
+function search() {
+    let value = searchInput.value.toLowerCase();
+    for (let item of list.children) {
+        let itemText = item.textContent.toLowerCase();
+        item.style.display = itemText.includes(value) ? 'flex' : 'none';
+    }
+}
+
+myButton.addEventListener('click', add);
+nameInput.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
         add();
     }
 });
+searchButton.addEventListener('click', search);
+searchInput.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        search();
+    }
+});
 
-myButton.addEventListener('click', add);
+searchInput.addEventListener('input', search);
 
+const closeModal = (event) => {
+    const target = event.target;
+    if (target === modalElem || (btnClose && target.closest(btnClose)) || event.code === 'Escape') {
+        modalElem.style.opacity = 0;
+        setTimeout(() => {
+            modalElem.style.visibility = 'hidden';
+        }, time);
+        window.removeEventListener('keydown', closeModal);
+    };
+}
+
+const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+    window.addEventListener('keydown', closeModal)
+};
+
+buttonElems.forEach( (btn) => {
+    btn.addEventListener('click', openModal);
+});
+
+modalElem.addEventListener('click', closeModal);
+
+document.addEventListener('click', function(event) {
+    if (event.target.matches('#ok')) {
+        list.innerHTML = '';
+        modalElem.style.opacity = 0;
+        setTimeout(() => {
+            modalElem.style.visibility = 'hidden';
+        }, time);
+        window.removeEventListener('keydown', closeModal);
+    }
+});
